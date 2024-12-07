ba5102ed361d2eaf6a38ffa094b26f1adc14aee8
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-07 13:24:46 +0100

    feat: init 03-contavocali-ex & add function

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,29 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+// const word = prompt('inserisci una parola sulla quale vuoi contare le vocali');
+const word = 'javascript'
 
 // Dichiara la funzione qui.
+function vowelsCount(word) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let vowelsInWord = [];
+    for (let i = 0; i < word.length; i++) {
+    console.log(word[i]);
+        if (
+            word[i] === "a" ||
+            word[i] === "e" ||
+            word[i] === "i" ||
+            word[i] === "o" ||
+            word[i] === "u"
+        ) {
+        vowelsInWord.push(word[i])
+        }
+    }
+    return vowelsInWord
+}
 
+console.log(vowelsCount(word));
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
