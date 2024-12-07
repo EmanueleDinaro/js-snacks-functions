ac8beb5c326f3866843f1fdf705609421ad54de7
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-07 13:30:57 +0100

    init 04-saluti-ex & add function

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,21 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome e
+restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
+// const userName = 'Mario';
+const userName = prompt('Inserisci il tuo nome');
 
 
 // Dichiara la funzione qui.
+function hello(name) {
+    const hello = `ciao ${userName}`
+    return hello
+}
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(hello(userName))
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
