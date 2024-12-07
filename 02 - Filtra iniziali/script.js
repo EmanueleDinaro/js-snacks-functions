2cf467d3f46f000317cc89b2072321ed3bcda174
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-07 12:56:25 +0100

    feat: add function ex 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,17 +4,23 @@ restituisca un array contenente solo le parole che iniziano con quella lettera *
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
-const letterToFilter = prompt('Inserisci una lettera');
-
-const wordsFiltered = [];
-
-for (let i = 0; i < names.length; i++) {
-  if (names[i][0] === letterToFilter) {
-    wordsFiltered.push(names[i]);
-  }
+/**
+ * This function permits to filter by a letter any words into an array
+ * @param {string} words 
+ * @param {string} letter 
+ * @returns {string}
+ */
+function filterByLetter(words, letter) {
+    const wordsFiltered = [];
+    for (let i = 0; i < words.length; i++) {
+        if (words[i][0] === letter) {
+            wordsFiltered.push(words[i]);
+        }
+    }
+    return wordsFiltered
 }
 
-console.log(wordsFiltered);
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(filterByLetter(names, "A"));
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
